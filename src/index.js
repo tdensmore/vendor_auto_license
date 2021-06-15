dd4713417833fbf2678ddf9ddabdb176c34fcbb0
todd densmore <469399+tdensmore@users.noreply.github.com>
2021-06-15 12:56:39 -0400

    Initialize dependabot/npm_and_yarn/ini-1.3.8

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './client/App.css';
+import './client/App.mobile.css';
+import App from './client/App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
